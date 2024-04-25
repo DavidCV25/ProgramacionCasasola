b813a8398eb0e0bfa1e0e8a3d971dcc8dd5af7d3
Davidcv <al.david.casasola.vilchez@iesportada.org>
2024-04-25 10:23:10 +0200

    Add(E4,E5,E6)

---
+++ E4/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Ejercicio_4;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        string contenedor = args[0];
+
+        Console.WriteLine($"Contenido del fichero {Path.GetFileName(contenedor)}:");
+        Console.Write(File.ReadAllText(contenedor));
+    }
+}
---
+++ E6/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Ejercicio_6;
+
+public class Program
+{
+    public static void Main()
+    {
+        string narracionGolIniesta = "Le van a agarrar intenta irse se va Navas, vamos, se desmarca Torres,\nel mundo contra Navas, que tiene ahí velocidad para ir, consigue enviar la pelota\npara el tacon de Iniesta, llega Cesc, aparece de nuevo Navas q se desfonda,\nentrega el balón para Fernando Torres, prepara el centro,\nla pide Iniesta, el rechaze para Cesc, Cesc para Iniesta\nno hay fuera de juego, vamos Iniesta y\nGOOOOOOOLLLL!!!!\n";
+        CrearFichero("Texto.txt", narracionGolIniesta);
+        ListarFichero("Texto.txt");
+    }
+
+    public static void CrearFichero(string fi, string texto)
+    {
+        using (StreamWriter escritor = File.CreateText(fi))
+        {
+            escritor.WriteLine(texto);
+        }
+    }
+    
+    public static void ListarFichero(string fi)
+    {
+        Console.WriteLine(File.ReadAllText(Path.GetFullPath(fi)));
+    }
+
+}
