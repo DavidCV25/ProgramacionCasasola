5f2e3c1d3a41d5a470f2bf9c025a35c3c7d6759b
Davidcv <al.david.casasola.vilchez@iesportada.org>
2024-04-25 17:11:32 +0200

    Añadir(Ej 7, 8, 9)

---
+++ E9/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Ejercicio_9;
+
+using System;
+using System.Diagnostics;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {  
+        Stopwatch temp = new Stopwatch();
+        temp.Start();
+        DateTime fecha = DateTime.Now;      
+        string lector = File.ReadAllText("Fichero.txt");
+
+        using (StreamWriter escritor = File.CreateText("Fichero.txt"))
+        {
+            Console.WriteLine("Escriba lo que desee.");
+            string texto = Console.ReadLine()!.ToString();          
+            temp.Stop();
+            lector += $"\nAbierto el {fecha:dd/MM/yy} a las {fecha: HH:mm:ss} por {Environment.UserName} y ha estado abierto durante {temp.Elapsed.Minutes:00}min {temp.Elapsed.Seconds:00}s {temp.Elapsed.Milliseconds:0000}ms";
+            lector += $"\n Y ha escrito:\n {texto}\n";
+            escritor.Write(lector);
+        }
+    }
+}
